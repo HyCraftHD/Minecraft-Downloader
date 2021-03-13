3329d226ddff4bd6ec69950b0c3c5fd3fef8ec07
HyCraftHD <hycrafthd@live.de>
2021-03-13 23:57:22 +0100

    Refactor download stuff with proper exceptions

--- src/main/java/net/hycrafthd/minecraft_downloader/MinecraftDownloader.java
+++ src/main/java/net/hycrafthd/minecraft_downloader/MinecraftDownloader.java
@@ -1,6 +1,7 @@
 package net.hycrafthd.minecraft_downloader;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Optional;
 
 import com.google.gson.Gson;
@@ -12,40 +13,40 @@ import net.hycrafthd.minecraft_downloader.mojang_api.version_manifest.VersionMan
 import net.hycrafthd.minecraft_downloader.util.Util;
 
 public class MinecraftDownloader {
-
+	
 	public static final String VERSION_MANIFEST = "https://launchermeta.mojang.com/mc/game/version_manifest_v2.json";
-
+	
 	public static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
-
+	
 	static void launch(String version, File output) {
-
+		
+		final VersionManifestV2 manifest;
+		
 		try {
-			final String versionManifest = Util.downloadJson(VERSION_MANIFEST);
-
-			VersionManifestV2 manifest = GSON.fromJson(versionManifest, VersionManifestV2.class);
-
-			final Optional<Version> foundVersionOptional = manifest.getVersions().stream()
-					.filter(manifestVersion -> manifestVersion.getId().equals(version)).findAny();
-
-			if (foundVersionOptional.isPresent()) {
-				final Version foundVersion = foundVersionOptional.get();
-				Main.LOGGER.info(foundVersion);
-
-				final String clientJson = Util.downloadJson(foundVersion.getUrl(), foundVersion.getSha1());
-
-				Client client = GSON.fromJson(clientJson, Client.class);
-
-				Main.LOGGER.info(clientJson);
-
-				Main.LOGGER.info(client);
-
-				Main.LOGGER.info(clientJson.length());
-			}
-
-		} catch (Exception e) {
-			e.printStackTrace();
+			manifest = GSON.fromJson(Util.downloadJson(VERSION_MANIFEST), VersionManifestV2.class);
+		} catch (IOException ex) {
+			Main.LOGGER.fatal("Could not download / parse version manifest json", ex);
+			return;
 		}
-
+		
+		final Optional<Version> foundVersionOptional = manifest.getVersions().stream().filter(manifestVersion -> manifestVersion.getId().equals(version)).findAny();
+		
+		if (!foundVersionOptional.isPresent()) {
+			Main.LOGGER.error("The requested version {} was not found in the version manifest json", version);
+			return;
+		}
+		
+		final Version foundVersion = foundVersionOptional.get();
+		
+		final Client client;
+		
+		try {
+			client = GSON.fromJson(Util.downloadJson(foundVersion.getUrl(), foundVersion.getSha1()), Client.class);
+		} catch (IOException ex) {
+			Main.LOGGER.fatal("Could not download / parse version manifest json", ex);
+			return;
+		}
+		
+		// TODO download libraries and other stuff
 	}
-
 }
