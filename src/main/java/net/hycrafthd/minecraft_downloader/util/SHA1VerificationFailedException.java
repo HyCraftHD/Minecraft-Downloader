7ecd4d5db6a0211ab55a594cda4a4220c3dfb1b6
HyCraftHD <hycrafthd@live.de>
2021-03-13 23:10:38 +0100

    Update util to check sha1

---
+++ src/main/java/net/hycrafthd/minecraft_downloader/util/SHA1VerificationFailedException.java
@@ -0,0 +1,12 @@
+package net.hycrafthd.minecraft_downloader.util;
+
+import java.io.IOException;
+
+public class SHA1VerificationFailedException extends IOException {
+	
+	private static final long serialVersionUID = 1L;
+	
+	public SHA1VerificationFailedException() {
+	}
+	
+}
--- src/main/java/net/hycrafthd/minecraft_downloader/util/Util.java
+++ src/main/java/net/hycrafthd/minecraft_downloader/util/Util.java
@@ -1,31 +1,81 @@
 package net.hycrafthd.minecraft_downloader.util;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.URL;
 import java.net.URLConnection;
 import java.nio.charset.StandardCharsets;
+import java.security.DigestInputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.stream.Collectors;
 
 public class Util {
 	
-	public static String downloadJson(String url) throws IOException {
+	public static String downloadJson(String url) throws IOException, NoSuchAlgorithmException {
 		return downloadJson(url, null);
 	}
 	
-	public static String downloadJson(String url, String sha1) throws IOException {
+	public static String downloadJson(String url, String sha1) throws IOException, NoSuchAlgorithmException {
+		final URLConnection urlConnection = new URL(url).openConnection();
+		urlConnection.setConnectTimeout(5000);
+		urlConnection.setReadTimeout(5000);
+		urlConnection.connect();
+		
+		final MessageDigest messageDigest = MessageDigest.getInstance("SHA1");
+		final String json;
+		
+		try (final BufferedReader reader = new BufferedReader(new InputStreamReader(new DigestInputStream(urlConnection.getInputStream(), messageDigest), StandardCharsets.UTF_8))) {
+			json = reader.lines().collect(Collectors.joining("\n"));
+		}
+		
+		if (sha1 != null) {
+			if (!bytesToHex(messageDigest.digest()).equals(sha1)) {
+				throw new SHA1VerificationFailedException();
+			}
+		}
+		
+		return json;
+	}
+	
+	public static void downloadFile(String url, String sha1, File output) throws IOException, NoSuchAlgorithmException {
 		final URLConnection urlConnection = new URL(url).openConnection();
 		urlConnection.connect();
 		
-		try (final BufferedReader reader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream(), StandardCharsets.UTF_8))) {
-			return reader.lines().collect(Collectors.joining("\n"));
+		final MessageDigest messageDigest = MessageDigest.getInstance("SHA1");
+		
+		final byte buffer[] = new byte[2048];
+		
+		try (final InputStream inputStream = new DigestInputStream(urlConnection.getInputStream(), messageDigest); //
+				final OutputStream outputStream = new FileOutputStream(output)) {
+			int count;
+			while ((count = inputStream.read(buffer)) != -1) {
+				outputStream.write(buffer, 0, count);
+			}
+		}
+		
+		if (sha1 != null) {
+			if (!bytesToHex(messageDigest.digest()).equals(sha1)) {
+				throw new SHA1VerificationFailedException();
+			}
 		}
 	}
 	
-	public static void downloadFile(String url, String sha1) throws IOException {
-		final URLConnection urlConnection = new URL(url).openConnection();
-		urlConnection.connect();
+	private static final byte[] HEX_ARRAY = "0123456789ABCDEF".getBytes(StandardCharsets.US_ASCII);
+	
+	public static String bytesToHex(byte[] bytes) {
+		byte[] hexChars = new byte[bytes.length * 2];
+		for (int j = 0; j < bytes.length; j++) {
+			int v = bytes[j] & 0xFF;
+			hexChars[j * 2] = HEX_ARRAY[v >>> 4];
+			hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
+		}
+		return new String(hexChars, StandardCharsets.UTF_8);
 	}
 	
 }
