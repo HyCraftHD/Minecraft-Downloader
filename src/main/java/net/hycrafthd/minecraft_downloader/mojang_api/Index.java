416be8c3227b3c25713e9bed6e2e15193eee7bac
HyCraftHD <hycrafthd@live.de>
2021-03-14 21:35:18 +0100

    Add basic assets download start

---
+++ src/main/java/net/hycrafthd/minecraft_downloader/mojang_api/Index.java
@@ -0,0 +1,13 @@
+package net.hycrafthd.minecraft_downloader.mojang_api;
+
+
+public class Index {
+	
+	
+	
+	public static class AssetObject {
+		private String hash;
+		private String size;
+	}
+	
+}
