772c6c6f9fd9c0681a6707d97b6a0d3e59e53a29
HyCraftHD <hycrafthd@live.de>
2021-03-14 05:18:46 +0100

    Add basic os util

---
+++ src/main/java/net/hycrafthd/minecraft_downloader/util/OSUtil.java
@@ -0,0 +1,46 @@
+package net.hycrafthd.minecraft_downloader.util;
+
+import java.util.Locale;
+
+public class OSUtil {
+	
+	public static final OS CURRENT_OS;
+	
+	static {
+		final String system = System.getProperty("os.name").toLowerCase(Locale.ROOT);
+		
+		if (system.contains("win")) {
+			CURRENT_OS = OS.WINDOWS;
+		} else if (system.contains("nix") || system.contains("nux") || system.contains("aix")) {
+			CURRENT_OS = OS.LINUX;
+		} else if (system.contains("mac")) {
+			CURRENT_OS = OS.OSX;
+		} else {
+			CURRENT_OS = OS.LINUX;
+		}
+	}
+	
+	public enum OS {
+		
+		WINDOWS("windows", "windows"),
+		LINUX("linux", "linux"),
+		OSX("osx", "macos");
+		
+		private final String name;
+		private final String classifierName;
+		
+		private OS(String name, String classifierName) {
+			this.name = name;
+			this.classifierName = classifierName;
+		}
+		
+		public String getName() {
+			return name;
+		}
+		
+		public String getClassifierName() {
+			return classifierName;
+		}
+		
+	}
+}
