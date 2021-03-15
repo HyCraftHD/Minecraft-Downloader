86a85fcc79fbf4c2044b00afa2504be288a88116
HyCraftHD <hycrafthd@live.de>
2021-03-15 23:13:54 +0100

    Add arguments parser

---
+++ src/main/java/net/hycrafthd/minecraft_downloader/launch/ArgumentsParser.java
@@ -0,0 +1,23 @@
+package net.hycrafthd.minecraft_downloader.launch;
+
+import java.util.List;
+
+import net.hycrafthd.minecraft_downloader.mojang_api.CurrentClientJson.ArgumentsJson;
+
+public class ArgumentsParser {
+	
+	private final String[] gameArgs;
+	
+	public ArgumentsParser(ArgumentsJson arguments, ProvidedSettings provided) {
+		gameArgs = parseArguments(arguments.getGameArguments(), provided);
+	}
+	
+	private final String[] parseArguments(List<String> arguments, ProvidedSettings provided) {
+		return arguments.stream().map(argument -> provided.replaceVariable(argument)).toArray(String[]::new);
+	}
+	
+	public String[] getGameArgs() {
+		return gameArgs;
+	}
+	
+}
