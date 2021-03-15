14f9666ba768e080693a14f96249a86acb0832a7
HyCraftHD <hycrafthd@live.de>
2021-03-15 23:35:56 +0100

    Remove method not needed

--- src/main/java/net/hycrafthd/minecraft_downloader/util/StringUtil.java
+++ src/main/java/net/hycrafthd/minecraft_downloader/util/StringUtil.java
@@ -2,10 +2,6 @@ package net.hycrafthd.minecraft_downloader.util;
 
 public class StringUtil {
 	
-	public static String replaceVersion(String string, String value) {
-		return replaceVariable("version", string, value);
-	}
-	
 	public static String replaceVariable(String variable, String string, String value) {
 		return string.replace("${" + variable + "}", value);
 	}
