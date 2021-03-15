7eda4d2a963d4130fcb1022924940ff8cc302a59
HyCraftHD <hycrafthd@live.de>
2021-03-15 15:04:14 +0100

    Add replace variable

--- src/main/java/net/hycrafthd/minecraft_downloader/launch/ProvidedSettings.java
+++ src/main/java/net/hycrafthd/minecraft_downloader/launch/ProvidedSettings.java
@@ -3,6 +3,7 @@ package net.hycrafthd.minecraft_downloader.launch;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
 
 public class ProvidedSettings {
@@ -21,6 +22,19 @@ public class ProvidedSettings {
 	
 	public void addVariable(LauncherVariables variable, String value) {
 		variables.put(variable, value);
+	}
+	
+	public String replaceVariable(String string) {
+		String output = string;
+		
+		for (Entry<LauncherVariables, String> entry : variables.entrySet()) {
+			final LauncherVariables variable = entry.getKey();
+			final String value = entry.getValue();
+			
+			output = variable.replaceVariable(output, value);
+		}
+		
+		return output;
 	}
 	
 }
