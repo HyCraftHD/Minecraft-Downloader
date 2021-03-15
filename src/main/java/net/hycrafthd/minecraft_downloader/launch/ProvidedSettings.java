9d319879896477a0800be85f8b13796d07f2e689
HyCraftHD <hycrafthd@live.de>
2021-03-15 14:54:19 +0100

    Add provided settings class

---
+++ src/main/java/net/hycrafthd/minecraft_downloader/launch/ProvidedSettings.java
@@ -0,0 +1,26 @@
+package net.hycrafthd.minecraft_downloader.launch;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class ProvidedSettings {
+	
+	private final Set<LauncherFeatures> features;
+	private final Map<LauncherVariables, String> variables;
+	
+	public ProvidedSettings() {
+		this.features = new HashSet<>();
+		this.variables = new HashMap<>();
+	}
+	
+	public void addFeature(LauncherFeatures feature) {
+		features.add(feature);
+	}
+	
+	public void addVariable(LauncherVariables variable, String value) {
+		variables.put(variable, value);
+	}
+	
+}
