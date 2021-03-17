99da3c6b2b01f3b55b84928f7919ab1323235501
HyCraftHD <hycrafthd@live.de>
2021-03-17 21:10:41 +0100

    Add generated settings for downloadable files

--- src/main/java/net/hycrafthd/minecraft_downloader/settings/GeneratedSettings.java
+++ src/main/java/net/hycrafthd/minecraft_downloader/settings/GeneratedSettings.java
@@ -1,10 +1,15 @@
 package net.hycrafthd.minecraft_downloader.settings;
 
+import java.util.List;
+
+import net.hycrafthd.minecraft_downloader.library.DownloadableFile;
 import net.hycrafthd.minecraft_downloader.mojang_api.CurrentClientJson;
 
 public class GeneratedSettings {
 	
 	private CurrentClientJson clientJson;
+	
+	private List<DownloadableFile> downloadableFiles;
 	
 	public void setClientJson(CurrentClientJson clientJson) {
 		this.clientJson = clientJson;
@@ -12,9 +17,20 @@ public class GeneratedSettings {
 	
 	public CurrentClientJson getClientJson() {
 		if (clientJson == null) {
-			throw new IllegalStateException("Client json is null");
+			throw new IllegalStateException("Client json is not set");
 		}
 		return clientJson;
+	}
+	
+	public void setDownloadableFiles(List<DownloadableFile> downloadableFiles) {
+		this.downloadableFiles = downloadableFiles;
+	}
+	
+	public List<DownloadableFile> getDownloadableFiles() {
+		if (downloadableFiles == null) {
+			throw new IllegalStateException("Downloadable files list is not set");
+		}
+		return downloadableFiles;
 	}
 	
 }
