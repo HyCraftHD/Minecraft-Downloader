1841205f236f5a92aa3dd27eafe74a712ae6d10f
HyCraftHD <hycrafthd@live.de>
2021-03-17 20:44:51 +0100

    Update minecraft parser

--- src/main/java/net/hycrafthd/minecraft_downloader/MinecraftParser.java
+++ src/main/java/net/hycrafthd/minecraft_downloader/MinecraftParser.java
@@ -7,22 +7,21 @@ import java.util.Optional;
 import net.hycrafthd.minecraft_downloader.mojang_api.CurrentClientJson;
 import net.hycrafthd.minecraft_downloader.mojang_api.VersionManifestJson;
 import net.hycrafthd.minecraft_downloader.mojang_api.VersionManifestJson.VersionJson;
+import net.hycrafthd.minecraft_downloader.settings.ProvidedSettings;
 import net.hycrafthd.minecraft_downloader.util.FileUtil;
 
 public class MinecraftParser {
 	
-	static CurrentClientJson launch(String version, File output) {
+	static void launch(ProvidedSettings settings) {
 		Main.LOGGER.info("Start parsing json files");
 		
-		final VersionJson foundVersion = getVersionOfManifest(version);
-		final CurrentClientJson client = getClientJson(foundVersion, output);
+		parseClientJson(extractVersionOfManifest(settings.getVersion()), settings);
 		
 		Main.LOGGER.info("Finished parsing json files");
-		return client;
 	}
 	
-	private static VersionJson getVersionOfManifest(String version) {
-		Main.LOGGER.info("Download and load version manifest");
+	private static VersionJson extractVersionOfManifest(String version) {
+		Main.LOGGER.info("Download and parse version manifest");
 		
 		final VersionManifestJson manifest;
 		
@@ -41,13 +40,13 @@ public class MinecraftParser {
 		return foundVersionOptional.get();
 	}
 	
-	private static CurrentClientJson getClientJson(VersionJson foundVersion, File output) {
-		Main.LOGGER.info("Download and extract client json");
+	private static void parseClientJson(VersionJson foundVersion, ProvidedSettings settings) {
+		Main.LOGGER.info("Download and parse client json");
 		
 		final CurrentClientJson client;
 		
 		try {
-			final File file = new File(output, Constants.CLIENT_JSON);
+			final File file = settings.getClientJsonFile();
 			
 			FileUtil.downloadFile(foundVersion.getUrl(), file, foundVersion.getSha1());
 			
@@ -56,7 +55,7 @@ public class MinecraftParser {
 			throw new IllegalStateException("Could not download / parse client json", ex);
 		}
 		
-		return client;
+		settings.getGeneratedSettings().setClientJson(client);
 	}
 	
 }
