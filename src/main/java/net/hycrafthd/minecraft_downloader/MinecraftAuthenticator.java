936a5983569f6b0c7fd12a5e68767bde991b53a2
HyCraftHD <hycrafthd@live.de>
2021-03-18 19:18:27 +0100

    Fix auth

---
+++ src/main/java/net/hycrafthd/minecraft_downloader/MinecraftAuthenticator.java
@@ -0,0 +1,11 @@
+package net.hycrafthd.minecraft_downloader;
+
+import net.hycrafthd.minecraft_downloader.settings.ProvidedSettings;
+
+public class MinecraftAuthenticator {
+	
+	static void launch(ProvidedSettings settings) {
+		Main.LOGGER.info("Start the authenticator to log into minecraft");
+	}
+	
+}
