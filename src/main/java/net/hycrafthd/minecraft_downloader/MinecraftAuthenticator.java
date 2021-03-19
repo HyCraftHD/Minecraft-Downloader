b86429d528e7120c502e91c574f5069eefa98f24
HyCraftHD <hycrafthd@live.de>
2021-03-19 17:51:00 +0100

    Fix authenticator

--- src/main/java/net/hycrafthd/minecraft_downloader/MinecraftAuthenticator.java
+++ src/main/java/net/hycrafthd/minecraft_downloader/MinecraftAuthenticator.java
@@ -1,5 +1,6 @@
 package net.hycrafthd.minecraft_downloader;
 
+import net.hycrafthd.minecraft_downloader.auth.api.MinecraftAuth;
 import net.hycrafthd.minecraft_downloader.settings.ProvidedSettings;
 
 public class MinecraftAuthenticator {
@@ -9,8 +10,23 @@ public class MinecraftAuthenticator {
 		
 		final ClassLoader classLoader = settings.getGeneratedSettings().getClassLoader();
 		
-		System.out.println(classLoader);
-		
+		try {
+			final Class<? extends MinecraftAuth> authImplClass = Class.forName("net.hycrafthd.minecraft_downloader.auth.impl.MinecraftAuthImpl2", true, classLoader).asSubclass(MinecraftAuth.class);
+			final MinecraftAuth auth = authImplClass.getConstructor(String.class, String.class).newInstance(username, password);
+			
+			System.out.println(authImplClass.getClassLoader());
+			System.out.println(auth.getClass().getClassLoader());
+			System.out.println(Class.forName("net.hycrafthd.minecraft_downloader.auth.impl.MinecraftAuthImpl2", false, classLoader));
+			
+			auth.logIn();
+			
+			System.out.println(auth.getAuthenticatedToken());
+			System.out.println(auth.getUUID());
+			System.out.println(auth.getName());
+			System.out.println(auth.getUserType());
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			throw new IllegalStateException("An exception occured during authentication", ex);
+		}
 	}
-	
 }
