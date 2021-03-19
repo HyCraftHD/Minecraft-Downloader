6c634e80d21499bdba77bf5e49ffa25165fbde89
HyCraftHD <hycrafthd@live.de>
2021-03-19 19:48:35 +0100

    Log classpath entries

--- src/main/java/net/hycrafthd/minecraft_downloader/MinecraftClasspathBuilder.java
+++ src/main/java/net/hycrafthd/minecraft_downloader/MinecraftClasspathBuilder.java
@@ -16,6 +16,14 @@ public class MinecraftClasspathBuilder {
 	static void launch(ProvidedSettings settings) {
 		Main.LOGGER.info("Start the classpath builder");
 		
+		createClassLoader(settings);
+		
+		Main.LOGGER.info("Finished the classpath builder");
+	}
+	
+	private static void createClassLoader(ProvidedSettings settings) {
+		Main.LOGGER.info("Find all jars and create classloader");
+		
 		final GeneratedSettings generatedSettings = settings.getGeneratedSettings();
 		
 		final List<URL> classPath = Stream.concat(Stream.of(settings.getClientJarFile(), settings.getAuthImplFile()), generatedSettings.getDownloadableFiles() //
@@ -31,12 +39,15 @@ public class MinecraftClasspathBuilder {
 					}
 				}).collect(Collectors.toList());
 		
+		Main.LOGGER.debug("The classpath entries are: ");
+		classPath.forEach(url -> {
+			Main.LOGGER.debug(" " + url);
+		});
+		
 		final MinecraftClassLoader classLoader = new MinecraftClassLoader(classPath.stream().toArray(URL[]::new));
 		
 		generatedSettings.setClassPath(classPath);
 		generatedSettings.setClassLoader(classLoader);
-		
-		Main.LOGGER.info("Finished the classpath builder");
 	}
 	
 	private static class MinecraftClassLoader extends URLClassLoader {
