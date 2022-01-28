c19f02cc668c5c3b7ead3f6476d5b84cdd6f3287
HyCraftHD <hycrafthd@live.de>
2022-01-29 00:09:17 +0100

    Update minecraft autentication

--- src/main/java/net/hycrafthd/minecraft_downloader/MinecraftAuthenticator.java
+++ src/main/java/net/hycrafthd/minecraft_downloader/MinecraftAuthenticator.java
@@ -1,13 +1,20 @@
 package net.hycrafthd.minecraft_downloader;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.NoSuchElementException;
+import java.util.UUID;
 
 import net.hycrafthd.logging_util.LoggingUtil;
 import net.hycrafthd.minecraft_authenticator.login.AuthenticationException;
-import net.hycrafthd.minecraft_authenticator.login.AuthenticationFile;
 import net.hycrafthd.minecraft_authenticator.login.Authenticator;
 import net.hycrafthd.minecraft_authenticator.login.User;
+import net.hycrafthd.minecraft_authenticator.login.file.AuthenticationFile;
+import net.hycrafthd.minecraft_authenticator.microsoft.service.MicrosoftService;
 import net.hycrafthd.minecraft_downloader.settings.LauncherVariables;
 import net.hycrafthd.minecraft_downloader.settings.ProvidedSettings;
 
@@ -16,21 +23,32 @@ public class MinecraftAuthenticator {
 	public static void launch(ProvidedSettings settings, File authFile, boolean authenticate, String authenticateType) {
 		Main.LOGGER.info("Start the authenticator to log into minecraft");
 		
-		if (authenticate) {
-			if (authenticateType == null || authenticateType.isEmpty() || authenticateType.equals("console")) {
-				consoleAuthentication(authFile);
-			} else {
-				throw new IllegalArgumentException("Authentication type is not known. Only console is supported right now");
-			}
-		}
-		
 		try {
-			final AuthenticationFile startAuthFile = AuthenticationFile.read(authFile.toPath());
-			final Authenticator authenticator = Authenticator.of(startAuthFile).shouldAuthenticate().run();
+			final AuthenticationFile startAuthFile;
+			
+			if (authenticate) {
+				if (authenticateType == null || authenticateType.isEmpty() || authenticateType.equals("console")) {
+					startAuthFile = consoleAuthentication();
+				} else {
+					throw new IllegalArgumentException("Authentication type is not known. Only 'console' is supported");
+				}
+			} else {
+				try (final FileInputStream inputStream = new FileInputStream(authFile)) {
+					startAuthFile = AuthenticationFile.read(inputStream);
+				}
+			}
+			
+			final Authenticator authenticator = Authenticator.of(startAuthFile) //
+					.shouldAuthenticate() //
+					.serviceConnectTimeout(10000) //
+					.serviceReadTimeout(10000) //
+					.run();
 			
 			final AuthenticationFile updatedAuthFile = authenticator.getResultFile();
 			if (!startAuthFile.equals(updatedAuthFile)) {
-				updatedAuthFile.write(authFile.toPath());
+				try (final FileOutputStream outputStream = new FileOutputStream(authFile)) {
+					updatedAuthFile.write(outputStream);
+				}
 			}
 			
 			final User user = authenticator.getUser().get();
@@ -43,18 +61,35 @@ public class MinecraftAuthenticator {
 			settings.addVariable(LauncherVariables.USER_TYPE, user.getType());
 			
 			Main.LOGGER.info("Logged into minecraft account");
-		} catch (AuthenticationException | IOException ex) {
+		} catch (AuthenticationException | IOException | NoSuchElementException ex) {
 			Main.LOGGER.info("An error occured while trying to log into minecraft account", ex);
 		}
 	}
 	
-	private static void consoleAuthentication(File authFile) {
+	private static AuthenticationFile consoleAuthentication() throws IOException, AuthenticationException {
 		Main.LOGGER.info("Authentication in console");
-		try {
-			// TODO Implement the console login in a better way than calling an others libraries main function
-			net.hycrafthd.minecraft_authenticator.Main.main(new String[] { "--auth-file", authFile.toPath().toString() });
-		} catch (Exception ex) {
-			Main.LOGGER.info("An error occured while authentication", ex);
+		
+		try (final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+			while (true) {
+				System.out.println("Type 'microsoft' or 'mojang' for account type");
+				final String type = reader.readLine();
+				
+				if (type.equals("microsoft")) {
+					System.out.println("Open the following link and log into your microsoft account");
+					System.out.println(MicrosoftService.oAuthLoginUrl());
+					System.out.println("Paste the code parameter of the returned url");
+					final String authCode = reader.readLine();
+					
+					return Authenticator.ofMicrosoft(authCode).serviceConnectTimeout(10000).serviceReadTimeout(10000).run().getResultFile();
+				} else if (type.equals("mojang")) {
+					System.out.println("Type in your username / email");
+					final String username = reader.readLine();
+					System.out.println("Type in your password");
+					final String password = reader.readLine();
+					
+					return Authenticator.ofYggdrasil(UUID.randomUUID().toString(), username, password).serviceConnectTimeout(10000).serviceReadTimeout(10000).run().getResultFile();
+				}
+			}
 		}
 	}
 }
