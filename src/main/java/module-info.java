19e349bf6fe27f190718aa63a1d219e05d15a130
HyCraftHD <hycrafthd@live.de>
2023-03-05 00:53:39 +0100

    Fixup modules

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,10 +7,14 @@ module net.hycrafthd.minecraft_downloader {
 	exports net.hycrafthd.minecraft_downloader.util;
 	
 	requires transitive com.google.gson;
+	
 	requires jopt.simple;
+	
 	requires net.hycrafthd.minecraft_authenticator;
 	requires net.hycrafthd.simple_minecraft_authenticator;
+	
 	requires transitive org.apache.logging.log4j;
 	requires org.apache.logging.log4j.iostreams;
+	
 	requires org.tukaani.xz;
 }
